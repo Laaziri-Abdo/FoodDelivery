8e3e1ee35fe21d88e2a019ebb3ad9f883cce49d6
Laaziri-Abdo <abdolaaziri@gmail.com>
2022-05-18 09:32:21 +0100

    version finale

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-const configdb = require('./config')['database']
-
-module.exports = new Sequelize(
-    configdb.database,
-    configdb.username,
-    configdb.password,
-    configdb
-)
